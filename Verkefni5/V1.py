83826a38a0dc9d5cb57d9dc5ef03901950af7587
Pálmi C Rúnarsson <pancakeprins@gmail.com>
2018-09-04 10:07:18 +0000

    Edit

--- Verkefni5/V1.py
+++ Verkefni5/V1.py
@@ -1,6 +1,6 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 max_int = 0
-while num_int <= 0:
+while num_int >= 0:
     if max_int < num_int:
         max_int = num_int
     num_int = int(input("Input a number: "))
